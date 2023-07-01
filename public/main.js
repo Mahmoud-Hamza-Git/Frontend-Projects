a4f59e69f897cd013bff9799f4cb518e7116b2bb
Mahmoud-Hamza-Git <mahmoudhamza9440@gmail.com>
2023-07-01 10:12:15 +0300

    edit

--- public/main.js
+++ public/main.js
@@ -1,11 +1,11 @@
 /*///////////////
 /// GLOBALS ///
 */ //////////////
-const socket = io("/");
-const user_1 = document.getElementById("user-1");
-const user_2 = document.getElementById("user-2");
-const btnCamera = document.getElementById("camera-btn");
-const btnMic = document.getElementById("mic-btn");
+const socket = io('/');
+const user_1 = document.getElementById('user-1');
+const user_2 = document.getElementById('user-2');
+const btnCamera = document.getElementById('camera-btn');
+const btnMic = document.getElementById('mic-btn');
 const constraints = {
   video: {
     width: { min: 640, ideal: 1920, max: 1920 },
@@ -16,14 +16,14 @@ const constraints = {
 let roomID = ROOMID;
 // check if the user enters direct to the room.html page without going to the lobby.
 if (!roomID) {
-  window.location = "/";
+  window.location = '/';
 }
 
 let localStream, remoteStream, peerConnction;
 const servers = {
   iceServers: [
     {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
     },
   ],
 };
@@ -38,73 +38,68 @@ let init = async () => {
   user_1.srcObject = localStream;
 
   // listening to events
-  socket.on("MemberJoined", (user2_ID) => {
-    console.log("2nd Member Joined:", user2_ID);
-    handleMemberJoined(user2_ID);
+  socket.on('MemberJoined', (user2_ID) => {
+    console.log('2nd Member Joined:', user2_ID);
+    createOffer(user2_ID);
   });
-  socket.on("MessageSent", (message, otherPeer_ID) => {
+  socket.on('MessageSent', (message, otherPeer_ID) => {
     handleMessageSent(message, otherPeer_ID);
   });
-  socket.on("MemberLeft", (memberLeftID) => {
-    console.log("Member", memberLeftID, "Left");
+  socket.on('MemberLeft', (memberLeftID) => {
+    console.log('Member', memberLeftID, 'Left');
     handleMemberLeft();
   });
 
   // join the room
-  socket.emit("join-room", ROOMID);
+  socket.emit('join-room', ROOMID);
 };
 
 ///////////////////
 // EVENT HANDLERS
 //////////////////
-let handleMemberJoined = async (user2_ID) => {
-  createOffer(user2_ID);
-};
-
 let handleMessageSent = async (message, otherPeer_ID) => {
   message = JSON.parse(message);
-  if (message.type === "offer") {
+  if (message.type === 'offer') {
     createAnswer(otherPeer_ID, message.offer); // here we will add the offer then create an answer and send it back to peer-1
   }
-  if (message.type === "answer") {
+  if (message.type === 'answer') {
     addAnswer(message.answer); //we will not need the userID here as we will not send any messages back
   }
-  if (message.type === "candidate") {
+  if (message.type === 'candidate') {
     // check if we have a connection then add these ice candidates
     if (peerConnction) {
       peerConnction.addIceCandidate(message.candidate);
     }
   }
-  console.log(message.type, " Message Sent");
+  console.log(message.type, ' Message Sent');
 };
 
 let handleMemberLeft = async () => {
-  user_2.style.display = "none";
-  user_1.classList.remove("small-frame");
-  console.log(`the user with the id: left`);
+  user_2.style.display = 'none';
+  user_1.classList.remove('small-frame');
 };
 
 let toggleCamera = async () => {
   // get the video track from the local stream and check if it is enabled or not
-  let videoTrack = localStream.getTracks().find((track) => track.kind === "video");
+  let videoTrack = localStream.getTracks().find((track) => track.kind === 'video');
   if (videoTrack.enabled) {
     videoTrack.enabled = false;
-    btnCamera.style.backgroundColor = "#df2c2ce8";
+    btnCamera.style.backgroundColor = '#df2c2ce8';
   } else {
     videoTrack.enabled = true;
-    btnCamera.style.backgroundColor = "#b366f9e6";
+    btnCamera.style.backgroundColor = '#b366f9e6';
   }
 };
 
 let toggleMic = async () => {
   let btnBackground = getComputedStyle(btnMic).backgroundColor; //get css values in javascript using getComputedStyle (as style.color only gets the inline style of the element)
-  if (btnBackground == "rgba(223, 42, 42, 0.91)") {
-    btnMic.style.backgroundColor = "rgba(178, 103, 249, 0.902)";
+  if (btnBackground == 'rgba(223, 42, 42, 0.91)') {
+    btnMic.style.backgroundColor = 'rgba(178, 103, 249, 0.902)';
   } else {
-    btnMic.style.backgroundColor = "rgba(223, 42, 42, 0.91)";
+    btnMic.style.backgroundColor = 'rgba(223, 42, 42, 0.91)';
   }
   // get the audio track from the local stream and check if it is enabled or not
-  let audioTrack = localStream.getTracks().find((track) => track.kind === "audio");
+  let audioTrack = localStream.getTracks().find((track) => track.kind === 'audio');
   if (audioTrack?.enabled) {
     audioTrack.enabled = false;
   } else if (audioTrack?.enabled) {
@@ -122,8 +117,8 @@ let createPeerConnection = async (MemberID) => {
   // create the remote stream then later we will add data to it.
   remoteStream = new MediaStream();
   user_2.srcObject = remoteStream;
-  user_2.style.display = "block";
-  user_1.classList.add("small-frame");
+  user_2.style.display = 'block';
+  user_1.classList.add('small-frame');
 
   // put all tracks(videos or audios) from localStream on the connection interface
   localStream.getTracks().forEach((track) => {
@@ -142,8 +137,8 @@ let createPeerConnection = async (MemberID) => {
     // why check if the current event is a candidate? we are listening already for ice candidate event
     if (event.candidate) {
       socket.emit(
-        "send-message",
-        JSON.stringify({ type: "candidate", candidate: event.candidate }),
+        'send-message',
+        JSON.stringify({ type: 'candidate', candidate: event.candidate }),
         MemberID
       );
     }
@@ -158,7 +153,7 @@ let createOffer = async (user2_ID) => {
   peerConnction.setLocalDescription(offer);
 
   // send message with the offer to the peer with that MemberID(we can send not only a string message but we can send the offer object after stringify it)
-  socket.emit("send-message", JSON.stringify({ type: "offer", offer: offer }), user2_ID); // (Member id of peer 2)
+  socket.emit('send-message', JSON.stringify({ type: 'offer', offer: offer }), user2_ID); // (Member id of peer 2)
 };
 
 // take the offer and create an answer and send it back to the other peer (notice that the remote peer tracks will be added to the peer connection interface in this step, as in createPeerConnection() function we will extract tracks from localStream(which is considered remote now relative peer-1 which we started with))
@@ -171,7 +166,7 @@ let createAnswer = async (otherPeer_ID, offer) => {
   await peerConnction.setLocalDescription(answer);
 
   // send the message back to peer_1
-  socket.emit("send-message", JSON.stringify({ type: "answer", answer: answer }), otherPeer_ID);
+  socket.emit('send-message', JSON.stringify({ type: 'answer', answer: answer }), otherPeer_ID);
 };
 
 // add the answer to remote discription of peer_1
@@ -186,8 +181,8 @@ let addAnswer = async (answer) => {
 ///////////////////
 // EVENT LISTENERS
 //////////////////
-btnCamera.addEventListener("click", toggleCamera);
-btnMic.addEventListener("click", toggleMic);
+btnCamera.addEventListener('click', toggleCamera);
+btnMic.addEventListener('click', toggleMic);
 
 ///////////////////
 // STARTING THE PAGE
--- server.js
+++ server.js
@@ -1,61 +1,57 @@
 /*
-1- initialize the environment and install dependancies
-  a- npm init -y
-  b- npm i express socket.io ejs uuid
-  c- npm i --save-div nodemon
-  d- set the structure of the app(view engine - static foldre - git and .gitignore - handle first get request and use uuid)
 ?? can i use the old params ? in a server app instead of routes with : params
 2- send the user to the lobby to take the room id then redirect him to the route of the room and render the room
 ?? can i move direct between html pages without going to route in server to render it if i'm using view engine or i have to create a truly html page not .ejs
 */
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
-const { v4: uuidV4 } = require("uuid");
+const httpServer = require('http').createServer(app);
+
+const { v4: uuidV4 } = require('uuid');
 
 // set the view engine
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // res.render("lobby", { roomId: uuidV4() });
-  res.render("lobby");
+  res.render('lobby');
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
 });
 
 /*
  */
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
+const expressServer = app.listen(4000, (err) => {
+  if (!err) console.log('server start at port 4000....');
+});
+
+//the io must created from the server you are working on(whatever it was express server comes from app.listen or httpserver comes from http.createServer())
+const io = require('socket.io')(expressServer);
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    console.log('rooms before any room: ', socket.rooms);
     socket.join(roomId);
-    let user2_ID = socket.id;
-    socket.to(roomId).emit("MemberJoined", user2_ID); // can i change it to send private message to that specific user?? no as the user joined may be myself so i only want to emit this event to other users joined
+    let user2_ID = socket.id; //this is user_1 and user_2 but only user_2 will receive the 'memberJoined' event so i named it user_2
+    socket.to(roomId).emit('MemberJoined', user2_ID); // can i change it to send private message to that specific user?? no as the user joined may be myself so i only want to emit this event to other users joined
 
-    console.log("user: ", user2_ID, "joined the room"); //actually user1 and user2 will be loged here but in the previous line only user2_ID will be send
+    console.log('user: ', user2_ID, 'joined the room'); //actually user1 and user2 will be loged here but in the previous line only user2_ID will be send
   });
 
-  socket.on("send-message", (message, receiver) => {
+  socket.on('send-message', (message, receiver) => {
     let sender = socket.id;
-    io.to(receiver).emit("MessageSent", message, sender); //send the message to the user with the MemberId sent in the event
-    console.log("\n", socket.id, " send a message");
+    io.to(receiver).emit('MessageSent', message, sender); //send the message to the user with the MemberId sent in the event
+    console.log('\n', socket.id, ' send a message');
   });
 
-  socket.on("disconnecting", (reason) => {
+  socket.on('disconnecting', (reason) => {
     for (const room of socket.rooms) {
       if (room !== socket.id) {
-        socket.to(room).emit("MemberLeft", socket.id);
+        socket.to(room).emit('MemberLeft', socket.id); //i could send also the room_id direct and pass it to socket.to() but if the user joined room the room_id is held in the socket.rooms so no need to resend it.
       }
     }
   });
-});
-/*
-
-*/
-httpServer.listen(4000, (err) => {
-  if (!err) console.log("server start at port 4000....");
 });
